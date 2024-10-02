924d839b29a9eb5435a646a28e132f54c02d2604
LuisICJ <luisortizdevillalba@gmail.com>
2024-10-02 16:47:02 +0200

    Añadida funcionalidad

--- src/main/java/site/luisicj/ejerciciocalculadora/CalculadoraCtl.java
+++ src/main/java/site/luisicj/ejerciciocalculadora/CalculadoraCtl.java
@@ -35,6 +35,32 @@ public class CalculadoraCtl {
 
     @FXML
     void onBtCalcularClick(ActionEvent event) {
+        double resultado;
+
+        try {
+            double op1 = Double.parseDouble(tfOperando1.getText());
+            double op2 = Double.parseDouble(tfOperando2.getText());
+
+            if (rbOperadorMas.isSelected()) {
+                resultado = op1 + op2;
+            } else if (rbOperadorMenos.isSelected()) {
+                resultado = op1 - op2;
+            } else if (rbOperadorMul.isSelected()) {
+                resultado = op1 * op2;
+            } else {
+
+                if (op2 == 0) {
+                    lbResultado.setText("No divida entre 0");
+                    return;
+                }
+
+                resultado = op1 / op2;
+            }
+
+            lbResultado.setText(String.valueOf(resultado));
+        } catch (NumberFormatException e) {
+            lbResultado.setText("Operando inválido");
+        }
     }
 
 }
